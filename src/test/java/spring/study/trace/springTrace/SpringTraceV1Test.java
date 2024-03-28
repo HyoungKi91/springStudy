95fe962ba5a661a50f687b059ef02ebb81bb61b6
hkkim <hkkim@aju.co.kr>
2024-03-28 10:44:33 +0900

    v1로그 테스트 코드 구현

--- src/test/java/spring/study/trace/springTrace/SpringTraceV1Test.java
+++ src/test/java/spring/study/trace/springTrace/SpringTraceV1Test.java
@@ -1,4 +1,17 @@
+package spring.study.trace.springTrace;
+
+import org.junit.jupiter.api.Test;
+import spring.study.trace.TraceStatus;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class SpringTraceV1Test {
-  
+
+    @Test
+    void begin_end(){
+        SpringTraceV1 traceV1 = new SpringTraceV1();
+        TraceStatus status = traceV1.begin("SpringHello");
+        traceV1.end(status);
+    }
+
 }
