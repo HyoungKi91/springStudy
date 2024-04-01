55432e869319ba4f9a07466c767dffa37b4d6270
hkkim <hkkim@aju.co.kr>
2024-04-01 11:05:43 +0900

    LogTrace test

---
+++ src/test/java/spring/study/trace/logTrace/FieldLogTraceTest.java
@@ -0,0 +1,28 @@
+package spring.study.trace.logTrace;
+
+import org.junit.jupiter.api.Test;
+import spring.study.trace.TraceStatus;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FieldLogTraceTest {
+
+    FieldLogTrace fieldLogTrace = new FieldLogTrace();
+    @Test
+    void FieldLogTraceTest(){
+        TraceStatus status1 = fieldLogTrace.begin("hello1");
+        TraceStatus status2 = fieldLogTrace.begin("hello2");
+        fieldLogTrace.end(status2);
+        fieldLogTrace.end(status1);
+    }
+
+    @Test
+    void FieldLogTraceExceptionTest(){
+        TraceStatus status1 = fieldLogTrace.begin("hello1");
+        TraceStatus status2 = fieldLogTrace.begin("hello2");
+        fieldLogTrace.exception(status2 , new IllegalStateException());
+        fieldLogTrace.exception(status1 , new IllegalStateException());
+    }
+
+
+}
